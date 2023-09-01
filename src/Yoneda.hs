886b2eead80f990f5d433b3b7dd27b168e3fb6d9
Magnus Møller <magnus108@me.com>
2023-09-01 10:50:22 +0200

    flopper

--- src/Yoneda.hs
+++ src/Yoneda.hs
@@ -19,19 +19,16 @@ instance Applicative f => Applicative (Yoneda f) where
 optimizeFmap :: (a -> b) -> [a] -> Yoneda [] b
 optimizeFmap f xs = Yoneda (\g -> fmap (g . f) xs)
 
-myYoneda :: [Int] -> Yoneda [] [Int]
-myYoneda xs = pure xs
-
 -- Your map functions
 naiveMap :: [Int] -> [Int]
-naiveMap xs = map (+ 1) (map (* 2) xs)
+naiveMap xs = map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) $ map (+ 1) (map (* 2) xs)
 
 yonedaMap :: [Int] -> [Int]
-yonedaMap xs = runYoneda (fmap (+ 1) (optimizeFmap (* 2) xs)) id
+yonedaMap xs = runYoneda (fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) $ fmap (+ 1) (optimizeFmap (* 2) xs)) id
 
 mainer :: IO ()
 mainer =
   defaultMain
-    [ bench "naive map" $ nf naiveMap [1 .. 1000000],
-      bench "yoneda map" $ nf yonedaMap [1 .. 1000000]
+    [ bench "naive map" $ whnf naiveMap [1 .. 1000000],
+      bench "yoneda map" $ whnf yonedaMap [1 .. 1000000]
     ]
