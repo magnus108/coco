2f953c2815c5ad52dbb3cd96180b47c3c79ad46b
Magnus Møller <magnus108@me.com>
2023-09-04 12:59:02 +0200

    ada

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   # name = "project-name";
-  compiler-nix-name = "ghc928"; # Version of GHC to use
+  compiler-nix-name = "ghc8107"; # Version of GHC to use
 
   crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
     p.mingwW64
@@ -12,7 +12,7 @@
 
   # Tools to include in the development shell
 #  shell.tools.profiteur = "latest";
-  shell.tools.cabal = "latest";
-  shell.tools.hlint = "latest";
-  shell.tools.haskell-language-server = "latest";
+#  shell.tools.cabal = "latest";
+#  shell.tools.hlint = "latest";
+#  shell.tools.haskell-language-server = "latest";
 }
