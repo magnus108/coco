10fa16a6839f2d8a3c8104c082db84658e1218e5
Magnus Møller <magnus108@me.com>
2023-09-13 16:47:45 +0200

    damn this nice

--- src/SpecIt2.hs
+++ src/SpecIt2.hs
@@ -33,105 +33,77 @@ cancel' :: PrepaymentState -> Maybe PrepaymentState
 cancel' (CloneState x1 x2) = Just $ PrepaymentState x1
 cancel' (PrepaymentState x) = Nothing
 
-data TkState
-  = Empty
-  | One PrepaymentState
+data Empty = Empty
+  deriving stock (Show, Eq, Ord)
+
+instance Arbitrary Empty where
+  arbitrary =
+    oneof
+      [ return Empty
+      ]
+
+data TkStateOne
+  = One PrepaymentState
   | OneSelected PrepaymentState
-  | Two PrepaymentState PrepaymentState
+  deriving stock (Show, Eq, Ord)
+
+instance Arbitrary TkStateOne where
+  arbitrary =
+    oneof
+      [ One <$> arbitrary,
+        OneSelected <$> arbitrary
+      ]
+
+data TkStateTwo
+  = Two PrepaymentState PrepaymentState
   | TwoOneSelected PrepaymentState PrepaymentState
   | TwoTwoSelected PrepaymentState PrepaymentState
   deriving stock (Show, Eq, Ord)
 
-instance Arbitrary TkState where
+instance Arbitrary TkStateTwo where
   arbitrary =
     oneof
-      [ return Empty,
-        One <$> arbitrary,
-        OneSelected <$> arbitrary,
-        Two <$> arbitrary <*> arbitrary,
+      [ Two <$> arbitrary <*> arbitrary,
         TwoOneSelected <$> arbitrary <*> arbitrary,
         TwoTwoSelected <$> arbitrary <*> arbitrary
       ]
 
-clear :: TkState -> TkState
-clear ts = Empty
+clearOne :: TkStateOne -> Empty
+clearOne ts = Empty
 
-addOne :: TkState -> Maybe TkState
-addOne Empty = Just $ One empty'
-addOne _ = Nothing
+clearTwo :: TkStateTwo -> Empty
+clearTwo ts = Empty
 
-canAddOne :: TkState -> Bool
-canAddOne Empty = True
-canAddOne _ = False
+addOne :: Empty -> TkStateOne
+addOne xs = One empty'
 
-addTwo :: TkState -> Maybe TkState
-addTwo (One x) = Just $ Two x empty'
-addTwo (OneSelected x) = Just $ TwoOneSelected x empty'
-addTwo _ = Nothing
+addTwo :: TkStateOne -> TkStateTwo
+addTwo (One x) = Two x empty'
+addTwo (OneSelected x) = TwoOneSelected x empty'
 
-canAddTwo :: TkState -> Bool
-canAddTwo (One x) = True
-canAddTwo (OneSelected x) = True
-canAddTwo _ = False
+deleteTwo :: TkStateTwo -> TkStateOne
+deleteTwo (Two x1 x2) = One x1
+deleteTwo (TwoOneSelected x1 x2) = OneSelected x1
 
-deleteTwo :: TkState -> Maybe TkState
-deleteTwo (Two x1 x2) = Just $ One x1
-deleteTwo (TwoOneSelected x1 x2) = Just $ OneSelected x1
-deleteTwo _ = Nothing
+flipOne :: TkStateOne -> TkStateOne
+flipOne (One x1) = OneSelected x1
+flipOne (OneSelected x1) = One x1
 
-deletable :: TkState -> Bool
-deletable (Two x1 x2) = True
-deletable (TwoOneSelected x1 x2) = True
-deletable _ = False
+selectTwoOne :: TkStateTwo -> TkStateTwo
+selectTwoOne (Two x1 x2) = TwoOneSelected x1 x2
+selectTwoOne (TwoOneSelected x1 x2) = Two x1 x2
+selectTwoOne (TwoTwoSelected x1 x2) = TwoOneSelected x1 x2
 
-selectOne :: TkState -> Maybe TkState
-selectOne (One x1) = Just $ OneSelected x1
-selectOne (Two x1 x2) = Just $ TwoOneSelected x1 x2
-selectOne (TwoTwoSelected x1 x2) = Just $ TwoOneSelected x1 x2
-selectOne _ = Nothing
-
-canSelectOne :: TkState -> Bool
-canSelectOne (One x1) = True
-canSelectOne (Two x1 x2) = True
-canSelectOne (TwoTwoSelected x1 x2) = True
-canSelectOne _ = False
-
-selectTwo :: TkState -> Maybe TkState
-selectTwo (Two x1 x2) = Just $ TwoTwoSelected x1 x2
-selectTwo (TwoOneSelected x1 x2) = Just $ TwoTwoSelected x1 x2
-selectTwo _ = Nothing
-
-canSelectTwo :: TkState -> Bool
-canSelectTwo (Two x1 x2) = True
-canSelectTwo (TwoOneSelected x1 x2) = False
-canSelectTwo _ = False
-
-deselectOne :: TkState -> Maybe TkState
-deselectOne (OneSelected x1) = Just $ One x1
-deselectOne (TwoOneSelected x1 x2) = Just $ Two x1 x2
-deselectOne _ = Nothing
-
-canDeselectOne :: TkState -> Bool
-canDeselectOne (OneSelected x1) = True
-canDeselectOne (TwoOneSelected x1 x2) = True
-canDeselectOne _ = False
-
-deselectTwo :: TkState -> Maybe TkState
-deselectTwo (TwoTwoSelected x1 x2) = Just $ Two x1 x2
-deselectTwo _ = Nothing
-
-canDeselectTwo :: TkState -> Bool
-canDeselectTwo (TwoTwoSelected x1 x2) = True
-canDeselectTwo _ = False
+selectTwoTwo :: TkStateTwo -> TkStateTwo
+selectTwoTwo (Two x1 x2) = TwoTwoSelected x1 x2
+selectTwoTwo (TwoOneSelected x1 x2) = TwoTwoSelected x1 x2
+selectTwoTwo (TwoTwoSelected x1 x2) = Two x1 x2
 
 --- Edit prepayment
-editOne :: TkState -> Maybe TkState
+{-
+editOne :: TkStateOne -> TkStateOne
 editOne (One x1) = (\x1' -> One x1') <$> clone' x1
 editOne (OneSelected x1) = (\x1' -> OneSelected x1') <$> clone' x1
-editOne (Two x1 x2) = (\x1' -> Two x1' x2) <$> clone' x1
-editOne (TwoOneSelected x1 x2) = (\x1' -> TwoOneSelected x1' x2) <$> clone' x1
-editOne (TwoTwoSelected x1 x2) = (\x1' -> TwoTwoSelected x1' x2) <$> clone' x1
-editOne _ = Nothing
 
 editTwo :: TkState -> Maybe TkState
 editTwo (Two x1 x2) = (\x2' -> Two x1 x2') <$> clone' x2
@@ -166,40 +138,43 @@ confirmTwo (Two x1 x2) = (\x2' -> Two x1 x2') <$> confirm' x2
 confirmTwo (TwoOneSelected x1 x2) = (\x2' -> TwoOneSelected x1 x2') <$> confirm' x2
 confirmTwo (TwoTwoSelected x1 x2) = (\x2' -> TwoTwoSelected x1 x2') <$> confirm' x2
 confirmTwo _ = Nothing
-
--- instance Observe TkState (Maybe TkState) (Maybe TkState) where
---  observe x y = if (canAddOne x && canAddOne y) then y else Nothing
+-}
 
 main :: IO ()
 main =
   quickSpec
-    [ "clear" `con` (clear :: TkState -> TkState),
-      "addOne" `con` (addOne :: TkState -> Maybe TkState),
-      "addTwo" `con` (addTwo :: TkState -> Maybe TkState),
-      "deleteTwo" `con` (deleteTwo :: TkState -> Maybe TkState),
-      "selectOne" `con` (selectOne :: TkState -> Maybe TkState),
-      "selectTwo" `con` (selectTwo :: TkState -> Maybe TkState),
-      "deselectOne" `con` (deselectOne :: TkState -> Maybe TkState),
-      "deselectTwo" `con` (deselectTwo :: TkState -> Maybe TkState),
+    [ "clearOne" `con` (clearOne :: TkStateOne -> Empty),
+      "clearTwo" `con` (clearTwo :: TkStateTwo -> Empty),
+      "addOne" `con` (addOne :: Empty -> TkStateOne),
+      "addTwo" `con` (addTwo :: TkStateOne -> TkStateTwo),
+      "deleteTwo" `con` (deleteTwo :: TkStateTwo -> TkStateOne),
+      "flipOne" `con` (flipOne :: TkStateOne -> TkStateOne),
+      "selectTwoOne" `con` (selectTwoOne :: TkStateTwo -> TkStateTwo),
+      "selectTwoTwo" `con` (selectTwoTwo :: TkStateTwo -> TkStateTwo),
+      {-
       "editOne" `con` (editOne :: TkState -> Maybe TkState),
       "editTwo" `con` (editTwo :: TkState -> Maybe TkState),
       "cancelOne" `con` (cancelOne :: TkState -> Maybe TkState),
       "cancelTwo" `con` (cancelTwo :: TkState -> Maybe TkState),
       "confirmOne" `con` (confirmOne :: TkState -> Maybe TkState),
       "confirmTwo" `con` (confirmTwo :: TkState -> Maybe TkState),
-      background
-        [ predicate "canAddOne" (canAddOne :: TkState -> Bool),
-          predicate "canAddTwo" (canAddTwo :: TkState -> Bool),
-          predicate "deletable" (deletable :: TkState -> Bool),
-          predicate "canSelectOne" (canSelectOne :: TkState -> Bool),
-          predicate "canSelectTwo" (canSelectTwo :: TkState -> Bool),
-          predicate "canDeselectOne" (canDeselectOne :: TkState -> Bool),
-          predicate "canDeselectTwo" (canDeselectTwo :: TkState -> Bool),
-          predicate "isJust" (isJust :: Maybe TkState -> Bool),
-          predicate "==" ((==) :: TkState -> TkState -> Bool),
-          predicate "/=" ((/=) :: TkState -> TkState -> Bool)
-        ],
+      -}
+      {-background
+      [ predicate "canAddOne" (canAddOne :: TkState -> Bool),
+        predicate "canAddTwo" (canAddTwo :: TkState -> Bool),
+        predicate "deletable" (deletable :: TkState -> Bool),
+        predicate "canSelectOne" (canSelectOne :: TkState -> Bool),
+        predicate "canSelectTwo" (canSelectTwo :: TkState -> Bool),
+        predicate "canDeselectOne" (canDeselectOne :: TkState -> Bool),
+        predicate "canDeselectTwo" (canDeselectTwo :: TkState -> Bool),
+        predicate "isJust" (isJust :: Maybe TkState -> Bool),
+        predicate "==" ((==) :: TkState -> TkState -> Bool),
+        predicate "/=" ((/=) :: TkState -> TkState -> Bool)
+      ],
+      -}
       -- monoObserve @TkState,
-      mono @TkState,
+      mono @Empty,
+      mono @TkStateOne,
+      mono @TkStateTwo,
       withMaxTests 1000
     ]
