d4c6f614cc4577c92381307ec4cf2c35c2e69fc4
Magnus Møller <magnus108@me.com>
2023-09-13 15:47:12 +0200

    Revert "not want"
    This reverts commit 1d53d59208a000e0e4bb049be4e680fd12e5d50a.

--- src/SpecIt2.hs
+++ src/SpecIt2.hs
@@ -58,39 +58,39 @@ instance Arbitrary TkState where
 clear :: TkState -> TkState
 clear ts = Empty
 
-addOne :: TkState -> TkState
-addOne Empty = One empty'
-addOne xs = xs
+addOne :: TkState -> Maybe TkState
+addOne Empty = Just $ One empty'
+addOne _ = Nothing
 
 canAddOne :: TkState -> Bool
 canAddOne Empty = True
 canAddOne _ = False
 
-addTwo :: TkState -> TkState
-addTwo (One x) = Two x empty'
-addTwo (OneSelected x) = TwoOneSelected x empty'
-addTwo xs = xs
+addTwo :: TkState -> Maybe TkState
+addTwo (One x) = Just $ Two x empty'
+addTwo (OneSelected x) = Just $ TwoOneSelected x empty'
+addTwo _ = Nothing
 
 canAddTwo :: TkState -> Bool
 canAddTwo (One x) = True
 canAddTwo (OneSelected x) = True
 canAddTwo _ = False
 
-deleteTwo :: TkState -> TkState
-deleteTwo (Two x1 x2) = One x1
-deleteTwo (TwoOneSelected x1 x2) = OneSelected x1
-deleteTwo xs = xs
+deleteTwo :: TkState -> Maybe TkState
+deleteTwo (Two x1 x2) = Just $ One x1
+deleteTwo (TwoOneSelected x1 x2) = Just $ OneSelected x1
+deleteTwo _ = Nothing
 
 deletable :: TkState -> Bool
 deletable (Two x1 x2) = True
 deletable (TwoOneSelected x1 x2) = True
 deletable _ = False
 
-selectOne :: TkState -> TkState
-selectOne (One x1) = OneSelected x1
-selectOne (Two x1 x2) = TwoOneSelected x1 x2
-selectOne (TwoTwoSelected x1 x2) = TwoOneSelected x1 x2
-selectOne xs = xs
+selectOne :: TkState -> Maybe TkState
+selectOne (One x1) = Just $ OneSelected x1
+selectOne (Two x1 x2) = Just $ TwoOneSelected x1 x2
+selectOne (TwoTwoSelected x1 x2) = Just $ TwoOneSelected x1 x2
+selectOne _ = Nothing
 
 canSelectOne :: TkState -> Bool
 canSelectOne (One x1) = True
@@ -98,94 +98,94 @@ canSelectOne (Two x1 x2) = True
 canSelectOne (TwoTwoSelected x1 x2) = True
 canSelectOne _ = False
 
-selectTwo :: TkState -> TkState
-selectTwo (Two x1 x2) = TwoTwoSelected x1 x2
-selectTwo (TwoOneSelected x1 x2) = TwoTwoSelected x1 x2
-selectTwo xs = xs
+selectTwo :: TkState -> Maybe TkState
+selectTwo (Two x1 x2) = Just $ TwoTwoSelected x1 x2
+selectTwo (TwoOneSelected x1 x2) = Just $ TwoTwoSelected x1 x2
+selectTwo _ = Nothing
 
 canSelectTwo :: TkState -> Bool
 canSelectTwo (Two x1 x2) = True
 canSelectTwo (TwoOneSelected x1 x2) = False
 canSelectTwo _ = False
 
-deselectOne :: TkState -> TkState
-deselectOne (OneSelected x1) = One x1
-deselectOne (TwoOneSelected x1 x2) = Two x1 x2
-deselectOne xs = xs
+deselectOne :: TkState -> Maybe TkState
+deselectOne (OneSelected x1) = Just $ One x1
+deselectOne (TwoOneSelected x1 x2) = Just $ Two x1 x2
+deselectOne _ = Nothing
 
 canDeselectOne :: TkState -> Bool
 canDeselectOne (OneSelected x1) = True
 canDeselectOne (TwoOneSelected x1 x2) = True
 canDeselectOne _ = False
 
-deselectTwo :: TkState -> TkState
-deselectTwo (TwoTwoSelected x1 x2) = Two x1 x2
-deselectTwo xs = xs
+deselectTwo :: TkState -> Maybe TkState
+deselectTwo (TwoTwoSelected x1 x2) = Just $ Two x1 x2
+deselectTwo _ = Nothing
 
 canDeselectTwo :: TkState -> Bool
 canDeselectTwo (TwoTwoSelected x1 x2) = True
 canDeselectTwo _ = False
 
 --- Edit prepayment
-editOne :: TkState -> TkState
-editOne g@(One x1) = fromMaybe g $ (\x1' -> One x1') <$> clone' x1
-editOne g@(OneSelected x1) = fromMaybe g $ (\x1' -> OneSelected x1') <$> clone' x1
-editOne g@(Two x1 x2) = fromMaybe g $ (\x1' -> Two x1' x2) <$> clone' x1
-editOne g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x1' -> TwoOneSelected x1' x2) <$> clone' x1
-editOne g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x1' -> TwoTwoSelected x1' x2) <$> clone' x1
-editOne xs = xs
+editOne :: TkState -> Maybe TkState
+editOne (One x1) = (\x1' -> One x1') <$> clone' x1
+editOne (OneSelected x1) = (\x1' -> OneSelected x1') <$> clone' x1
+editOne (Two x1 x2) = (\x1' -> Two x1' x2) <$> clone' x1
+editOne (TwoOneSelected x1 x2) = (\x1' -> TwoOneSelected x1' x2) <$> clone' x1
+editOne (TwoTwoSelected x1 x2) = (\x1' -> TwoTwoSelected x1' x2) <$> clone' x1
+editOne _ = Nothing
 
-editTwo :: TkState -> TkState
-editTwo g@(Two x1 x2) = fromMaybe g $ (\x2' -> Two x1 x2') <$> clone' x2
-editTwo g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x2' -> TwoOneSelected x1 x2') <$> clone' x2
-editTwo g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x2' -> TwoTwoSelected x1 x2') <$> clone' x2
-editTwo xs = xs
+editTwo :: TkState -> Maybe TkState
+editTwo (Two x1 x2) = (\x2' -> Two x1 x2') <$> clone' x2
+editTwo (TwoOneSelected x1 x2) = (\x2' -> TwoOneSelected x1 x2') <$> clone' x2
+editTwo (TwoTwoSelected x1 x2) = (\x2' -> TwoTwoSelected x1 x2') <$> clone' x2
+editTwo _ = Nothing
 
-cancelOne :: TkState -> TkState
-cancelOne g@(One x1) = fromMaybe g $ (\x1' -> One x1') <$> cancel' x1
-cancelOne g@(OneSelected x1) = fromMaybe g $ (\x1' -> OneSelected x1') <$> cancel' x1
-cancelOne g@(Two x1 x2) = fromMaybe g $ (\x1' -> Two x1' x2) <$> cancel' x1
-cancelOne g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x1' -> TwoOneSelected x1' x2) <$> cancel' x1
-cancelOne g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x1' -> TwoTwoSelected x1' x2) <$> cancel' x1
-cancelOne xs = xs
+cancelOne :: TkState -> Maybe TkState
+cancelOne (One x1) = (\x1' -> One x1') <$> cancel' x1
+cancelOne (OneSelected x1) = (\x1' -> OneSelected x1') <$> cancel' x1
+cancelOne (Two x1 x2) = (\x1' -> Two x1' x2) <$> cancel' x1
+cancelOne (TwoOneSelected x1 x2) = (\x1' -> TwoOneSelected x1' x2) <$> cancel' x1
+cancelOne (TwoTwoSelected x1 x2) = (\x1' -> TwoTwoSelected x1' x2) <$> cancel' x1
+cancelOne _ = Nothing
 
-cancelTwo :: TkState -> TkState
-cancelTwo g@(Two x1 x2) = fromMaybe g $ (\x2' -> Two x1 x2') <$> cancel' x2
-cancelTwo g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x2' -> TwoOneSelected x1 x2') <$> cancel' x2
-cancelTwo g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x2' -> TwoTwoSelected x1 x2') <$> cancel' x2
-cancelTwo xs = xs
+cancelTwo :: TkState -> Maybe TkState
+cancelTwo (Two x1 x2) = (\x2' -> Two x1 x2') <$> cancel' x2
+cancelTwo (TwoOneSelected x1 x2) = (\x2' -> TwoOneSelected x1 x2') <$> cancel' x2
+cancelTwo (TwoTwoSelected x1 x2) = (\x2' -> TwoTwoSelected x1 x2') <$> cancel' x2
+cancelTwo _ = Nothing
 
-confirmOne :: TkState -> TkState
-confirmOne g@(One x1) = fromMaybe g $ (\x1' -> One x1') <$> confirm' x1
-confirmOne g@(OneSelected x1) = fromMaybe g $ (\x1' -> OneSelected x1') <$> confirm' x1
-confirmOne g@(Two x1 x2) = fromMaybe g $ (\x1' -> Two x1' x2) <$> confirm' x1
-confirmOne g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x1' -> TwoOneSelected x1' x2) <$> confirm' x1
-confirmOne g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x1' -> TwoTwoSelected x1' x2) <$> confirm' x1
-confirmOne xs = xs
+confirmOne :: TkState -> Maybe TkState
+confirmOne (One x1) = (\x1' -> One x1') <$> confirm' x1
+confirmOne (OneSelected x1) = (\x1' -> OneSelected x1') <$> confirm' x1
+confirmOne (Two x1 x2) = (\x1' -> Two x1' x2) <$> confirm' x1
+confirmOne (TwoOneSelected x1 x2) = (\x1' -> TwoOneSelected x1' x2) <$> confirm' x1
+confirmOne (TwoTwoSelected x1 x2) = (\x1' -> TwoTwoSelected x1' x2) <$> confirm' x1
+confirmOne _ = Nothing
 
-confirmTwo :: TkState -> TkState
-confirmTwo g@(Two x1 x2) = fromMaybe g $ (\x2' -> Two x1 x2') <$> confirm' x2
-confirmTwo g@(TwoOneSelected x1 x2) = fromMaybe g $ (\x2' -> TwoOneSelected x1 x2') <$> confirm' x2
-confirmTwo g@(TwoTwoSelected x1 x2) = fromMaybe g $ (\x2' -> TwoTwoSelected x1 x2') <$> confirm' x2
-confirmTwo xs = xs
+confirmTwo :: TkState -> Maybe TkState
+confirmTwo (Two x1 x2) = (\x2' -> Two x1 x2') <$> confirm' x2
+confirmTwo (TwoOneSelected x1 x2) = (\x2' -> TwoOneSelected x1 x2') <$> confirm' x2
+confirmTwo (TwoTwoSelected x1 x2) = (\x2' -> TwoTwoSelected x1 x2') <$> confirm' x2
+confirmTwo _ = Nothing
 
 main :: IO ()
 main =
   quickSpec
     [ "clear" `con` (clear :: TkState -> TkState),
-      "addOne" `con` (addOne :: TkState -> TkState),
-      "addTwo" `con` (addTwo :: TkState -> TkState),
-      "deleteTwo" `con` (deleteTwo :: TkState -> TkState),
-      "selectOne" `con` (selectOne :: TkState -> TkState),
-      "selectTwo" `con` (selectTwo :: TkState -> TkState),
-      "deselectOne" `con` (deselectOne :: TkState -> TkState),
-      "deselectTwo" `con` (deselectTwo :: TkState -> TkState),
-      "editOne" `con` (editOne :: TkState -> TkState),
-      "editTwo" `con` (editTwo :: TkState -> TkState),
-      "cancelOne" `con` (cancelOne :: TkState -> TkState),
-      "cancelTwo" `con` (cancelTwo :: TkState -> TkState),
-      "confirmOne" `con` (confirmOne :: TkState -> TkState),
-      "confirmTwo" `con` (confirmTwo :: TkState -> TkState),
+      "addOne" `con` (addOne :: TkState -> Maybe TkState),
+      "addTwo" `con` (addTwo :: TkState -> Maybe TkState),
+      "deleteTwo" `con` (deleteTwo :: TkState -> Maybe TkState),
+      "selectOne" `con` (selectOne :: TkState -> Maybe TkState),
+      "selectTwo" `con` (selectTwo :: TkState -> Maybe TkState),
+      "deselectOne" `con` (deselectOne :: TkState -> Maybe TkState),
+      "deselectTwo" `con` (deselectTwo :: TkState -> Maybe TkState),
+      "editOne" `con` (editOne :: TkState -> Maybe TkState),
+      "editTwo" `con` (editTwo :: TkState -> Maybe TkState),
+      "cancelOne" `con` (cancelOne :: TkState -> Maybe TkState),
+      "cancelTwo" `con` (cancelTwo :: TkState -> Maybe TkState),
+      "confirmOne" `con` (confirmOne :: TkState -> Maybe TkState),
+      "confirmTwo" `con` (confirmTwo :: TkState -> Maybe TkState),
       background
         [ predicate "canAddOne" (canAddOne :: TkState -> Bool),
           predicate "canAddTwo" (canAddTwo :: TkState -> Bool),
